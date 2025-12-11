e03e76cf8d9509a74c631d15c975dcf49256f171
mitpatel9 <patelmit2014@gmail.com>
2025-12-11 11:03:14 +0530

    oder tab

--- StyledComponents/MarketsStyled.js
+++ StyledComponents/MarketsStyled.js
@@ -57,7 +57,7 @@ export const MarketVol = styled.div`
   }
 `;
 export const MarketEndDate = styled.div`
- font-weight: 400;
+  font-weight: 400;
   font-size: 16px;
   color: #b5b5b5;
   .icons {
@@ -65,4 +65,178 @@ export const MarketEndDate = styled.div`
     height: 18px;
     margin-right: 0.5rem;
   }
+`;
+
+//oder buy sell section
+export const OrderContaier = styled.div`
+  padding: 1rem;
+  border: 2px solid #fd7979;
+  border-radius: 10px;
+`;
+export const OrderTopHedding = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  .image {
+    max-height: 58px;
+    max-width: 58px;
+    border-radius: 10px;
+  }
+`;
+export const OrderTopHedText = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+`;
+export const OrderTopSubText = styled.div`
+  font-weight: 600;
+  font-size: 12px;
+  span {
+    font-size: 14px;
+    color: #fd7979;
+  }
+`;
+export const OrderMenu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 1rem;
+  border-bottom: 1px solid #dbdbdb;
+`;
+export const OrderMenuOption = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+export const BuySellMenu = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+
+  &.active {
+    color: #fd7979;
+  }
+`;
+export const OrderOption = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+`;
+export const OrderButton = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 1rem;
+  gap: 0.5rem;
+`;
+export const OderYesBtn = styled.div`
+  width: 50%;
+  padding: 8px 16px;
+  font-weight: 600;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  border: 2px solid #fd7979;
+
+  &.active {
+    background: #fd7979;
+    color: #fff;
+  }
+`;
+export const OrderNoBtn = styled.div`
+  width: 50%;
+  font-weight: 600;
+  padding: 8px 16px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  border: 2px solid #fd7979;
+
+  &.active {
+    background: #fd7979;
+    color: #fff;
+  }
+`;
+export const OrderContract = styled.div`
+  border: 2px solid #fd7979;
+  border-radius: 10px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const ContracContainer = styled.div`
+  width: 70%;
+  padding: 1rem;
+`;
+export const ContracHedText = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+`;
+export const ContracIntrest = styled.div`
+  font-weight: 600;
+  font-size: 12px;
+  color: #fd7979;
+`;
+export const ContracInput = styled.div`
+  width: 30%;
+
+  input {
+    outline: none;
+    box-shadow: none;
+    border: none;
+   
+
+    &::placeholder {
+      color: #828282;
+      opacity: 0.7;
+      font-size: 28px;
+      font-weight: 600;
+    }
+
+    &:focus {
+      outline: none;
+      border: 0;
+      box-shadow: none;
+      font-size: 28px;
+      font-weight: 600;
+    }
+  }
+`;
+
+export const OrderEstimated = styled.div``;
+export const OrderEstimatedValue = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.3rem 0rem;
+`;
+export const LeftText = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+  color: #828282;
+`;
+export const RightValue = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+  color: #828282;
+
+  span {
+    color: #0ec286;
+  }
+`;
+export const OderTradeButton = styled.div`
+  width: 100%;
+  font-weight: 600;
+  padding: 8px 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  background: #fd7979;
+  color: #fff;
 `;
--- components/MarketLayout/OrderBuySell.js
+++ components/MarketLayout/OrderBuySell.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import {
+  BuySellMenu,
+  ContracContainer,
+  ContracHedText,
+  ContracInput,
+  ContracIntrest,
+  LeftText,
+  OderTradeButton,
+  OderYesBtn,
+  OrderButton,
+  OrderContaier,
+  OrderContract,
+  OrderEstimated,
+  OrderEstimatedValue,
+  OrderMenu,
+  OrderMenuLeft,
+  OrderMenuOption,
+  OrderMenuRight,
+  OrderNoBtn,
+  OrderOption,
+  OrderTopHedding,
+  OrderTopHedText,
+  OrderTopIcon,
+  OrderTopImg,
+  OrderTopSubText,
+  OrderTopTitle,
+  RightValue,
+} from "@/StyledComponents/MarketsStyled";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import React from "react";
+import Test from "../../asset/Image/test.jpg";
+import { Input } from "reactstrap";
+import { CommonDiv, Hr } from "@/StyledComponents/CommonStyled";
 
 const OrderBuySell = () => {
   return (
-    <div>
-      order bu and sell
-    </div>
-  )
-}
+    <OrderContaier>
+      <OrderTopHedding>
+        <Image src={Test} className="image" />
+        <CommonDiv>
+          <OrderTopHedText>Time 2025 Person of the Year</OrderTopHedText>
+          <OrderTopSubText>
+            <span>Buy Yes</span> · Los Angeles R
+          </OrderTopSubText>
+        </CommonDiv>
+      </OrderTopHedding>
 
-export default OrderBuySell
+      <OrderMenu>
+        <OrderMenuOption>
+          <BuySellMenu className="active">Buy</BuySellMenu>
+          <BuySellMenu>Sell</BuySellMenu>
+        </OrderMenuOption>
+        <OrderOption>
+          Limit <Icon icon="mingcute:down-fill" className="icons" />
+        </OrderOption>
+      </OrderMenu>
+
+      <OrderButton>
+        <OderYesBtn className="active">Yes 19¢</OderYesBtn>
+        <OrderNoBtn>No 19¢</OrderNoBtn>
+      </OrderButton>
+
+      <OrderContract>
+        <ContracContainer>
+          <ContracHedText>Contracts</ContracHedText>
+          <ContracIntrest>Earn 3.5% Interest</ContracIntrest>
+        </ContracContainer>
+        <ContracInput>
+          <Input placeholder="0" />
+        </ContracInput>
+      </OrderContract>
+
+      <OrderEstimated>
+        <OrderEstimatedValue>
+          <LeftText>Average price</LeftText>
+          <RightValue>19¢</RightValue>
+        </OrderEstimatedValue>
+        <OrderEstimatedValue>
+          <LeftText>Estimated cost</LeftText>
+          <RightValue>$19</RightValue>
+        </OrderEstimatedValue>
+        <OrderEstimatedValue>
+          <LeftText>
+            Payout If Yes <Icon icon="line-md:link" className="icons" />
+          </LeftText>
+          <RightValue>
+            $100 <span> (+$81) </span>
+          </RightValue>
+        </OrderEstimatedValue>
+      </OrderEstimated>
+
+      <OderTradeButton>Trade</OderTradeButton>
+    </OrderContaier>
+  );
+};
+
+export default OrderBuySell;
